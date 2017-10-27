d7c4d0ca440f54d1b3f93504f9a6e9a94f5bcdb3
Davide Delfanti <shatetiel@gmail.com>
2017-10-27 15:24:35 +0200

    tslint

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -1,13 +1,3 @@
 class Constants {
-<<<<<<< HEAD
-  // let USER_AGENT: string = '';
-
-  public static get USER_AGENT(): string {
-    return "WebScrob";
-  }
-
-}
-=======
   USER_AGENT: string = 'WebScrob'; 
 }
->>>>>>> e107b87926730a94493df717424859df38df8331
